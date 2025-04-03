f419f84641953e067d3f4bd8acdb0ebdb61c516f
jcesa <j.cesar.costa2013@outlook.com>
2025-04-03 19:35:20 -0300

    Voltando ao normal meu teste.

--- src/main/java/com/jc/concessionaria/controller/CarroController.java
+++ src/main/java/com/jc/concessionaria/controller/CarroController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/carros")
 @CrossOrigin(origins = "http://localhost:4200")
 public class CarroController {
 
